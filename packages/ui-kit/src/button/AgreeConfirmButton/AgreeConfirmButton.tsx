c878d4d33c268ee6d68d37355794294f046a65ed
hiroshi <hiroshi.tan57@gmail.com>
2023-01-30 12:40:34 +0000

    Add AgreeConfirmButton

---
+++ packages/ui-kit/src/button/AgreeConfirmButton/AgreeConfirmButton.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+import { Button, useTheme, PaletteColor } from '@mui/material';
+
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
+import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
+
+type AgreeConfirmButtonProps = {
+  fullWidth?: boolean;
+  withIcon?: boolean;
+  kind: 'agree' | 'disagree';
+  disabled?: boolean;
+  onClick(): void;
+};
+
+export function AgreeConfirmButton({
+  fullWidth,
+  withIcon,
+  kind,
+  disabled,
+  onClick,
+}: AgreeConfirmButtonProps) {
+  const theme = useTheme();
+
+  let label;
+  let startWith;
+  let color: 'red' | 'green' = 'red';
+
+  const lightBlueColor = (theme.palette[
+    'light-blue' as keyof typeof theme.palette
+  ] as PaletteColor).main;
+  const darkColor = (theme.palette[
+    'dark' as keyof typeof theme.palette
+  ] as PaletteColor).main;
+  const sxObj = {
+    width: fullWidth ? 'null' : 157,
+    textAlign: 'center',
+    textTransform: 'capitalize',
+  };
+
+  if (kind === 'agree') {
+    label = 'Agree';
+    color = 'green';
+    console.log(withIcon);
+    startWith = withIcon === true ? <ThumbUpAltOutlinedIcon /> : undefined;
+  }
+
+  if (kind === 'disagree') {
+    label = 'Disagree';
+    color = 'red';
+    startWith = withIcon ? <ThumbDownOutlinedIcon /> : undefined;
+  }
+
+  return (
+    <Button
+      disabled={disabled}
+      variant="contained"
+      onClick={onClick}
+      fullWidth={fullWidth}
+      startIcon={startWith}
+      color={color}
+      sx={{
+        ...sxObj,
+        '&.Mui-disabled': {
+          backgroundColor: lightBlueColor,
+          color: darkColor,
+          cursor: 'not-allowed',
+        },
+      }}
+    >
+      {label}
+    </Button>
+  );
+}
---
+++ packages/ui-kit/src/button/AgreeConfirmButton/index.ts
@@ -0,0 +1,1 @@
+export { AgreeConfirmButton } from './AgreeConfirmButton';
