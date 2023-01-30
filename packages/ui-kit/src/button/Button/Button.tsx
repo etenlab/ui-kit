d8c086304547c6b0ca7bc96e8a0c403be45d7b2a
hiroshi <hiroshi.tan57@gmail.com>
2023-01-30 12:45:25 +0000

    Modify components

--- packages/ui-kit/src/button/Button/Button.tsx
+++ packages/ui-kit/src/button/Button/Button.tsx
@@ -9,18 +9,21 @@ type ButtonProps = {
   withIcon?: boolean;
   children?: React.ReactNode;
   variant: 'primary' | 'secondary';
+  onClick(): void;
 };
 
 export function Button({
   variant,
   fullWidth = false,
   withIcon = false,
+  onClick,
   children,
 }: ButtonProps) {
   const variantType = variant === 'primary' ? 'contained' : 'outlined';
 
   return (
     <StyledButton
+      onClick={onClick}
       variant={variantType}
       fullWidth={fullWidth}
       endIcon={withIcon ? <ArrowForwardIcon /> : null}
---
+++ packages/ui-kit/src/button/index.ts
@@ -0,0 +1,3 @@
+export { Button } from './Button';
+export { AgreeConfirmButton } from './AgreeConfirmButton';
+export { PlusButton } from './PlusButton';
