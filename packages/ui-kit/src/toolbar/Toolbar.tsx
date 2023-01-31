81505ff64c27bb55e7b120a1fbcdfb7a80b0e34c
hiroshi <hiroshi.tan57@gmail.com>
2023-01-31 21:28:12 +0000

    Add toolbar

---
+++ packages/ui-kit/src/toolbar/Toolbar.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import { IconButton, Stack, Typography, Badge } from '@mui/material';
+import { BiMessageRounded, FiBell, FiMenu } from '../icons';
+
+type ToolbarProps = {
+  title: string;
+  isNewDiscussion?: boolean;
+  isNewNotification?: boolean;
+  buttons: {
+    discussion: boolean;
+    notification: boolean;
+  };
+  onClickDiscussionBtn(): void;
+  onClickNotificationBtn(): void;
+  onClickMenuBtn(): void;
+};
+
+export function Toolbar({
+  title,
+  isNewDiscussion,
+  isNewNotification,
+  buttons = {
+    discussion: true,
+    notification: true,
+  },
+  onClickDiscussionBtn,
+  onClickNotificationBtn,
+  onClickMenuBtn,
+}: ToolbarProps) {
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{ padding: '3px 20px' }}
+    >
+      <Typography variant="h3" sx={{ lineHeight: '22px', color: '#1B1B1B' }}>
+        {title}
+      </Typography>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        gap="4px"
+      >
+        {buttons.discussion ? (
+          <IconButton onClick={onClickDiscussionBtn}>
+            <Badge color="red" variant="dot" invisible={!isNewDiscussion}>
+              <BiMessageRounded
+                style={{
+                  color: '#5C6673',
+                  fontSize: 24,
+                  transform: 'rotateY(180deg)',
+                }}
+              />
+            </Badge>
+          </IconButton>
+        ) : null}
+        {buttons.notification ? (
+          <IconButton onClick={onClickNotificationBtn}>
+            <Badge color="red" variant="dot" invisible={!isNewNotification}>
+              <FiBell style={{ color: '#5C6673', fontSize: 24 }} />
+            </Badge>
+          </IconButton>
+        ) : null}
+        <IconButton onClick={onClickMenuBtn}>
+          <FiMenu style={{ color: '#5C6673', fontSize: '40px' }} />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
+}
