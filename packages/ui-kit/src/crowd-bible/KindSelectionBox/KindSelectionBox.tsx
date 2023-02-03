ee7ae47c466a6a62ff3528fffa92bdebd1872ad8
hiroshi <hiroshi.tan57@gmail.com>
2023-02-03 22:28:11 +0000

    Add KindSelectionModal

---
+++ packages/ui-kit/src/crowd-bible/KindSelectionBox/KindSelectionBox.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import { IconButton, PaletteColor, Stack, Typography } from '@mui/material';
+
+import { Button } from '../../button';
+import { FiX } from '../../icons';
+
+type KindSelectionBoxProps = {
+  title: string;
+  label: string;
+  onTextClick(): void;
+  onChapterClick(): void;
+  onVerseClick(): void;
+  onCancel(): void;
+};
+
+export function KindSelectionBox({
+  title,
+  label,
+  onTextClick,
+  onChapterClick,
+  onVerseClick,
+  onCancel,
+}: KindSelectionBoxProps) {
+  return (
+    <Stack gap="12px" sx={{ padding: '20px' }}>
+      <Stack>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography variant="h2">{title}</Typography>
+          <IconButton onClick={onCancel}>
+            <FiX />
+          </IconButton>
+        </Stack>
+        <Typography
+          variant="body3"
+          sx={{ color: (theme) => (theme.palette.gray as PaletteColor).main }}
+        >
+          {label}
+        </Typography>
+      </Stack>
+      <Button fullWidth color="dark" variant="secondary" onClick={onTextClick}>
+        Part of the Text (word, phrase, paragraph)
+      </Button>
+      <Button
+        fullWidth
+        color="dark"
+        variant="secondary"
+        onClick={onChapterClick}
+      >
+        Chapter
+      </Button>
+      <Button fullWidth color="dark" variant="secondary" onClick={onVerseClick}>
+        Verse
+      </Button>
+    </Stack>
+  );
+}
---
+++ stories/partial/crowd-bible/KindSelectionModal/KindSelectionBox.stories.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import {
+  ThemeProvider,
+  KindSelectionBox,
+} from '../../../../packages/ui-kit/src';
+
+export default {
+  title: 'Partial/Crowd Bible/KindSelectionBox',
+  component: KindSelectionBox,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '3em', background: '#eee', width: '500px' }}>
+        <ThemeProvider>
+          <Story />
+        </ThemeProvider>
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof KindSelectionBox>;
+
+const Template: ComponentStory<typeof KindSelectionBox> = (args) => (
+  <KindSelectionBox {...args} />
+);
+
+export const LeaveFeedbackBox = Template.bind({});
+LeaveFeedbackBox.args = {
+  title: 'Leave Feedback',
+  label: 'Choose what you want to leave feedback for:',
+  onTextClick: () => {
+    alert('Click Text Button');
+  },
+  onChapterClick: () => {
+    alert('Click Chapter Button');
+  },
+  onVerseClick: () => {
+    alert('Click Verse Button');
+  },
+  onCancel: () => {
+    alert('Click Cancel Button');
+  },
+};
+
+export const AskQuestion = Template.bind({});
+AskQuestion.args = {
+  title: 'Ask Question',
+  label: 'Сhoose what you want to leave asking for:',
+  onTextClick: () => {
+    alert('Click Text Button');
+  },
+  onChapterClick: () => {
+    alert('Click Chapter Button');
+  },
+  onVerseClick: () => {
+    alert('Click Verse Button');
+  },
+  onCancel: () => {
+    alert('Click Cancel Button');
+  },
+};
