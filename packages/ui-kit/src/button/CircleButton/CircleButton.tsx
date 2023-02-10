50d2c670f1bc8d32de00b1faba50123a7d8c37bd
hiroshi <hiroshi.tan57@gmail.com>
2023-02-10 22:12:44 +0000

    Add CircleButton

---
+++ packages/ui-kit/src/button/CircleButton/CircleButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { IconButton, Box, IconButtonProps, PaletteColor } from '@mui/material';
+
+export function CircleButton({
+  icon,
+  color = 'blue-primary',
+  circleSize = '34px',
+  onClick,
+  ...props
+}: IconButtonProps & {
+  icon: React.ReactNode;
+  circleSize?: string;
+}) {
+  return (
+    <IconButton onClick={onClick} color={color} {...props}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: '50%',
+          width: circleSize,
+          height: circleSize,
+          backgroundColor: (theme) =>
+            (theme.palette[color as keyof typeof theme.palette] as PaletteColor)
+              .main,
+        }}
+      >
+        {icon}
+      </Box>
+    </IconButton>
+  );
+}
---
+++ stories/primary/button/CircleButton.stories.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import {
+  ThemeProvider,
+  CircleButton,
+  BsFillPlayFill,
+  CiPause1,
+  colors,
+  Typography,
+} from '../../../packages/ui-kit/src';
+
+export default {
+  title: 'Primary/Button/CircleButton',
+  component: CircleButton,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '3em', background: '#eee', width: '500px' }}>
+        <ThemeProvider>
+          <Story />
+        </ThemeProvider>
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof CircleButton>;
+
+const Template: ComponentStory<typeof CircleButton> = (args) => (
+  <CircleButton {...args} />
+);
+
+export const PlayButton = Template.bind({});
+PlayButton.args = {
+  icon: (
+    <BsFillPlayFill
+      style={{ color: colors['white'], fontSize: 40, paddingLeft: 3 }}
+    />
+  ),
+  onClick: () => alert('Clicked Circle Button!'),
+};
+
+export const PauseButton = Template.bind({});
+PauseButton.args = {
+  icon: <CiPause1 style={{ color: colors['white'], strokeWidth: '1.2px' }} />,
+  onClick: () => alert('Clicked Circle Button!'),
+};
+
+export const TextButton = Template.bind({});
+TextButton.args = {
+  icon: (
+    <Typography
+      variant="body3"
+      sx={{ fontWeight: 800, color: colors['white'] }}
+    >
+      Aa
+    </Typography>
+  ),
+  color: 'green',
+  circleSize: '30px',
+  onClick: () => alert('Clicked Circle Button!'),
+};
