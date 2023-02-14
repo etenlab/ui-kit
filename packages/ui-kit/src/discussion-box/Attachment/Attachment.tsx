667602b9d4c3f24f78441cca30d726c1247ffdb2
hiroshi <hiroshi.tan57@gmail.com>
2023-02-14 00:00:00 +0000

    Add Attachment

---
+++ packages/ui-kit/src/discussion-box/Attachment/Attachment.tsx
@@ -0,0 +1,111 @@
+import React, { ReactElement } from 'react';
+
+import { IconButton, Box } from '@mui/material';
+
+import { FileViewer } from '../FileViewer';
+import { ImageViewer } from '../ImageViewer';
+import { VideoPlayer } from '../VideoPlayer';
+import { AudioPlayer } from '../AudioPlayer';
+
+import { IFile } from '../types';
+import { getMimeType } from '../utils';
+import { BiTrashAlt } from '../../icons';
+import { colors } from '../../ThemeProvider';
+
+type WrapperProps = {
+  onRemove(): void;
+  mode: 'view' | 'quill';
+  children: JSX.Element;
+};
+
+export function Wrapper({ onRemove, mode, children }: WrapperProps) {
+  const sxObj =
+    mode === 'view'
+      ? {
+          top: 0,
+          right: '-25px',
+        }
+      : {
+          top: '3px',
+          right: '3px',
+        };
+
+  return (
+    <Box
+      sx={{
+        display: 'inline-block',
+        position: 'relative',
+      }}
+    >
+      {children}
+      <IconButton
+        onClick={onRemove}
+        sx={{
+          position: 'absolute',
+          padding: '3px',
+          border: 'none',
+          background: `rgba(0, 0, 0, 0.5)`,
+          color: colors['white'],
+          ...sxObj,
+        }}
+      >
+        <BiTrashAlt style={{ fontSize: '15px' }} />
+      </IconButton>
+    </Box>
+  );
+}
+
+type AttachmentProps = {
+  file: IFile;
+  mode?: 'view' | 'quill';
+  onRemove(): void;
+};
+
+export function Attachment({ file, mode = 'view', onRemove }: AttachmentProps) {
+  const mime = getMimeType(file.file_type);
+
+  let content: ReactElement;
+
+  switch (mime) {
+    case 'video': {
+      content = (
+        <VideoPlayer
+          src={file.file_url}
+          file_type={file.file_type || ''}
+          mode={mode}
+        />
+      );
+      break;
+    }
+    case 'audio': {
+      content = (
+        <AudioPlayer
+          src={file.file_url}
+          file_type={file.file_type || ''}
+          mode={mode}
+        />
+      );
+      break;
+    }
+    case 'image': {
+      content = (
+        <ImageViewer
+          src={file.file_url}
+          file_name={file.file_name}
+          mode={mode}
+        />
+      );
+      break;
+    }
+    default: {
+      content = <FileViewer file={file} mode={mode} />;
+      break;
+    }
+  }
+
+  return (
+    <Wrapper onRemove={onRemove} mode={mode}>
+      {content}
+    </Wrapper>
+  );
+}
---
+++ stories/partial/discussion-box/Attachment/VideoPlayer.stories.tsx
@@ -0,0 +1,52 @@
+import React, { useState, MouseEvent } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ThemeProvider } from '../../../../packages/ui-kit/src';
+import { Attachment } from '../../../../packages/ui-kit/src/discussion-box';
+
+export default {
+  title: 'Partial/Discussion Box/Attachment/VideoPlayer',
+  component: Attachment,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          margin: '3em',
+          background: '#ccc',
+          width: '500px',
+          padding: '3em',
+        }}
+      >
+        <ThemeProvider>
+          <Story />
+        </ThemeProvider>
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Attachment>;
+
+const Template: ComponentStory<typeof Attachment> = (args) => (
+  <Attachment {...args} />
+);
+
+const file = {
+  id: 1,
+  file_name: 'movie.mp4',
+  file_type: 'video/mp4',
+  file_size: 1025334,
+  file_url: '/movie.mp4',
+};
+
+export const ViewMode = Template.bind({});
+ViewMode.args = {
+  file,
+  mode: 'view',
+  onRemove: () => alert('Click remove button!'),
+};
+
+export const QuillMode = Template.bind({});
+QuillMode.args = {
+  file,
+  mode: 'quill',
+  onRemove: () => alert('Click remove button!'),
+};
