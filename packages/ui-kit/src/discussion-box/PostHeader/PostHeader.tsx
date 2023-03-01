3869d7b26b65e3ec52534b8bb55f4aae38ada97c
hiroshi <hiroshi.tan57@gmail.com>
2023-03-01 21:11:35 +0000

    Add PostHeader

---
+++ packages/ui-kit/src/discussion-box/PostHeader/PostHeader.tsx
@@ -0,0 +1,35 @@
+import React, { MouseEvent } from 'react';
+import { Stack, IconButton } from '@mui/material';
+
+import { Avatar } from '../Avatar';
+import { Username } from '../Username';
+import { DateViewer } from '../DateViewer';
+import { BsFillRecordFill, BiDotsHorizontalRounded } from '../../icons';
+
+type PostHeaderProps = {
+  username: string;
+  avatar: string;
+  date: Date;
+  openActionList(event: MouseEvent<HTMLElement>): void;
+};
+
+export function PostHeader({
+  username,
+  avatar = '',
+  date,
+  openActionList,
+}: PostHeaderProps) {
+  return (
+    <Stack direction="row" justifyContent="space-between">
+      <Stack direction="row" gap="10px" alignItems="center">
+        <Avatar username={username} url={avatar} mini={false} />
+        <Username username={username} />
+        <BsFillRecordFill style={{ fontSize: '4px' }} />
+        <DateViewer date={date} />
+      </Stack>
+      <IconButton onClick={openActionList}>
+        <BiDotsHorizontalRounded />
+      </IconButton>
+    </Stack>
+  );
+}
