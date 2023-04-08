39ad50f966f6aa69accd2040c5496783ac0240c0
hardikpoudeldev <hardikpoudelupwork@gmail.com>
2023-04-08 12:21:17 -0400

    updated tab and entries page components with mui styles

--- packages/ui-kit/src/diegesis/Tab.tsx
+++ packages/ui-kit/src/diegesis/Tab.tsx
@@ -1,55 +1,57 @@
-import { Tab, Tabs, styled } from "@mui/material";
-import React from "react";
+import { Tab, Tabs, styled } from '@mui/material';
+import React from 'react';
 
 interface StyledTabsProps {
-    children?: React.ReactNode;
-    value: number;
-    onClick: (event: React.SyntheticEvent) => void;
-    onChange?: (event: React.SyntheticEvent, newValue: number) => void;
-    className?: string;
+  children?: React.ReactNode;
+  value: number;
+  onClick: (event: React.SyntheticEvent) => void;
+  onChange?: (event: React.SyntheticEvent, newValue: number) => void;
+  className?: string;
 }
 
 export const CustomTabs = styled((props: StyledTabsProps) => (
-    <Tabs
-        {...props}
-    />
+  <Tabs {...props} />
 ))(({ theme }) => ({
-    '& .MuiTabs-flexContainer': {
-        padding: '0px 1rem',
-    },
-    '& .MuiTabs-indicator': {
-        display: 'flex',
-        justifyContent: 'center',
-        backgroundColor: 'transparent',
-        top: '70%'
-    },
-    '& .MuiTabs-indicatorSpan': {
-        width: '100%',
-        backgroundColor: theme.palette.background['turquoise-light'],
-        margin: '0px 1rem',
-        height: '1px'
-    },
+  '& .MuiTabs-flexContainer': {
+    padding: '0px 1rem',
+  },
+  '& .MuiTabs-indicator': {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    top: '70%',
+  },
+  '& .MuiTabs-indicatorSpan': {
+    width: '100%',
+    backgroundColor: theme.palette.background['turquoise-light'],
+    margin: '0px 1rem',
+    height: '1px',
+  },
 }));
 
 interface StyledTabProps {
-    label: string;
-    value: number;
+  label: string;
+  value: number;
 }
 
 export const CustomTab = styled((props: StyledTabProps) => (
-    <Tab {...props} wrapped={true} />
+  <Tab {...props} wrapped={true} />
 ))(({ theme }) => ({
-    textTransform: 'none',
-    fontWeight: theme.typography.fontWeightRegular,
-    fontSize: '1.25rem',
-    textDecoration: 'underline',
-    textUnderlineOffset: '0.5rem',
-    textDecorationColor: theme.palette.text['turquoise-light'],
-    padding: '1rem',
-    paddingBottom: '2rem',
+  textTransform: 'none',
+  fontWeight: theme.typography.fontWeightRegular,
+  fontSize: '1.25rem',
+  textDecoration: 'underline',
+  textUnderlineOffset: '0.5rem',
+  textDecorationColor: theme.palette.text['turquoise-light'],
+  padding: '1rem',
+  paddingBottom: '2rem',
+  color: theme.palette.text['dark'],
+  '&.Mui-selected': {
+    backgroundColor: theme.palette.background['light-gray'],
     color: theme.palette.text['dark'],
-    '&.Mui-selected': {
-        backgroundColor: theme.palette.background['light-gray'],
-        color: theme.palette.text['dark'],
-    },
+  },
+  textOverflow: 'ellipsis',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '1rem',
+  },
 }));
--- packages/ui-kit/src/diegesis/entries/Entries.tsx
+++ packages/ui-kit/src/diegesis/entries/Entries.tsx
@@ -1,28 +1,43 @@
-import { Box, Container } from "@mui/material"
-import React, { useState } from "react"
-import PageFooter from "../PageFooter"
-import PageHeader from "../PageHeader"
-import SideNav from "../SideNav"
-import EntriesTopControls from "./EntriesTopControls"
-import EntriesDataTable from "./EntriesDataTable"
-import "./Entries.css"
+import { Box, Container, styled } from '@mui/material';
+import React, { useState } from 'react';
+import PageFooter from '../PageFooter';
+import PageHeader from '../PageHeader';
+import SideNav from '../SideNav';
+import EntriesTopControls from './EntriesTopControls';
+import EntriesDataTable from './EntriesDataTable';
+import './Entries.css';
 
-interface IProps {
-}
+interface IProps {}
 export function EntriesPage(_props: IProps) {
-    const [isSideNavOpen, setSideNavOpenStatus] = useState(false)
-    return (
-        <Box component={'div'} id="entries-page">
-            <PageHeader openSideNav={() => setSideNavOpenStatus(true)} />
-            <SideNav open={isSideNavOpen} close={() => { setSideNavOpenStatus(false) }} />
-            <Container className="controls-section">
-                <EntriesTopControls />
-            </Container>
-            <Container className="table-section">
-                <EntriesDataTable />
-            </Container>
-            <PageFooter />
-        </Box>
-    )
+  const [isSideNavOpen, setSideNavOpenStatus] = useState(false);
+  return (
+    <Box component={'div'}>
+      <PageHeader openSideNav={() => setSideNavOpenStatus(true)} />
+      <SideNav
+        open={isSideNavOpen}
+        close={() => {
+          setSideNavOpenStatus(false);
+        }}
+      />
+      <StyledControlsContainer>
+        <EntriesTopControls />
+      </StyledControlsContainer>
+      <Container className="table-section">
+        <EntriesDataTable />
+      </Container>
+      <PageFooter />
+    </Box>
+  );
 }
-export default EntriesPage
+
+//#region styled components
+const StyledControlsContainer = styled(Container)(({ theme }) => ({
+  paddingTop: '60px',
+  paddingBottom: '30px',
+  [theme.breakpoints.down('sm')]: {
+    paddingTop: '30px',
+  },
+}));
+//#endregion
+
+export default EntriesPage;
