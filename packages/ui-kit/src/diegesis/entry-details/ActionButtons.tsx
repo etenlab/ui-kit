9d90c5b17ad1d801f62287ef88d560d60ae9f416
hardikpoudeldev <hardikpoudelupwork@gmail.com>
2023-04-09 13:43:17 -0400

    updated entry detail page css with mui styles

--- packages/ui-kit/src/diegesis/entry-details/ActionButtons.tsx
+++ packages/ui-kit/src/diegesis/entry-details/ActionButtons.tsx
@@ -1,29 +1,55 @@
-import { Button, Stack } from "@mui/material";
-import React from "react";
-import { AiOutlineDownload, AiOutlineFileText } from "react-icons/ai";
+import { Button, Stack } from '@mui/material';
+import React from 'react';
+import { AiOutlineDownload, AiOutlineFileText } from 'react-icons/ai';
 
-type Props = {}
+type Props = {};
 export default function ActionButtons(_props: Props) {
-    return (
-        <Stack direction={'row'} alignItems={'center'} justifyContent={'flex-end'} className="full-width action-buttons">
-            <Button
-                sx={{ fontWeight: 700, fontSize: '1.3rem', textTransform: 'none', borderRadius: '2rem', padding: '0.8rem', minWidth: '12rem', marginRight: '1rem' }}
-                startIcon={<AiOutlineFileText />}
-                variant={'contained'}
-                color={'green'}
-                size={'large'}
-            >
-                View
-            </Button>
-            <Button
-                sx={{ fontWeight: 700, fontSize: '1.3rem', textTransform: 'none', borderRadius: '2rem', padding: '0.8rem', minWidth: '12rem' }}
-                startIcon={<AiOutlineDownload />}
-                variant={'contained'}
-                color={'green'}
-                size={'large'}
-            >
-                Download
-            </Button>
-        </Stack>
-    )
+  return (
+    <Stack
+      flexDirection={'row'}
+      alignItems={'center'}
+      justifyContent={'flex-end'}
+      sx={(theme) => ({
+        [theme.breakpoints.down('sm')]: {
+          marginTop: '1rem',
+          marginBottom: '1rem',
+          justifyContent: 'space-between',
+        },
+      })}
+    >
+      <Button
+        sx={{
+          fontWeight: 700,
+          fontSize: '1.3rem',
+          textTransform: 'none',
+          borderRadius: '2rem',
+          padding: '0.8rem',
+          width: '47%',
+          marginRight: '1rem',
+        }}
+        startIcon={<AiOutlineFileText />}
+        variant={'contained'}
+        color={'green'}
+        size={'large'}
+      >
+        View
+      </Button>
+      <Button
+        sx={{
+          fontWeight: 700,
+          fontSize: '1.3rem',
+          textTransform: 'none',
+          borderRadius: '2rem',
+          padding: '0.8rem',
+          width: '47%',
+        }}
+        startIcon={<AiOutlineDownload />}
+        variant={'contained'}
+        color={'green'}
+        size={'large'}
+      >
+        Download
+      </Button>
+    </Stack>
+  );
 }
--- packages/ui-kit/src/diegesis/entry-details/EntryDetails.tsx
+++ packages/ui-kit/src/diegesis/entry-details/EntryDetails.tsx
@@ -1,63 +1,154 @@
-import { Box, Container, Stack, Typography } from "@mui/material"
-import React, { useState } from "react"
-import PageFooter from "../PageFooter"
-import PageHeader from "../PageHeader"
-import SideNav from "../SideNav"
-import TopControls from "./TopControls"
-import DataTable, { HeadCell } from "../data-table/DataTable"
-import "./EntryDetails.css"
-import SelectOptions from "../SelectOptions"
-import ActionButtons from "./ActionButtons"
-import { BackButton } from "../BackButton"
+import { Box, Container, Stack, Typography, styled } from '@mui/material';
+import React, { useState } from 'react';
+import PageFooter from '../PageFooter';
+import PageHeader from '../PageHeader';
+import SideNav from '../SideNav';
+import TopControls from './TopControls';
+import DataTable, { HeadCell } from '../data-table/DataTable';
+import SelectOptions from '../SelectOptions';
+import ActionButtons from './ActionButtons';
+import { BackButton } from '../BackButton';
 
-interface IProps {
-}
+interface IProps {}
 interface IData {
-    key: string
-    value: string
+  key: string;
+  value: string;
 }
 const headCells: HeadCell[] = [
-    { id: 'key', disablePadding: true, label: '', numeric: false },
-    { id: 'value', disablePadding: false, label: '', numeric: false },
-    { id: 'emptyColumn1', disablePadding: false, label: '', numeric: false },
-]
+  { id: 'key', disablePadding: true, label: '', numeric: false },
+  { id: 'value', disablePadding: false, label: '', numeric: false },
+  { id: 'emptyColumn1', disablePadding: false, label: '', numeric: false },
+];
 const sampleData: IData[] = [
-    { key: 'Details', value: '' },
-    { key: 'Abbreviation', value: 'engBBE' },
-    { key: 'Copyright', value: 'engBBE' },
-    { key: 'Language', value: 'engBBE' },
-    { key: 'Data source', value: 'engBBE' },
-    { key: 'Owner', value: 'engBBE' },
-    { key: 'Entry ID', value: 'engBBE' },
-    { key: 'Revision', value: 'engBBE' },
-    { key: 'Content', value: '39 OT, 27 NT' },
-]
+  { key: 'Details', value: '' },
+  { key: 'Abbreviation', value: 'engBBE' },
+  { key: 'Copyright', value: 'engBBE' },
+  { key: 'Language', value: 'engBBE' },
+  { key: 'Data source', value: 'engBBE' },
+  { key: 'Owner', value: 'engBBE' },
+  { key: 'Entry ID', value: 'engBBE' },
+  { key: 'Revision', value: 'engBBE' },
+  { key: 'Content', value: '39 OT, 27 NT' },
+];
 export function EntryDetailPage(_props: IProps) {
-    const [isSideNavOpen, setSideNavOpenStatus] = useState(false)
-    return (
-        <Box component={'div'} id="entry-detail-page">
-            <PageHeader openSideNav={() => setSideNavOpenStatus(true)} />
-            <SideNav open={isSideNavOpen} close={() => { setSideNavOpenStatus(false) }} />
-            <Container className="controls-section">
-                <TopControls />
-            </Container>
-            <Container className="details-section">
-                <Stack direction={'row'} className="divider mt-2"></Stack>
-                <DataTable expandableRowOnMobile={false} headCells={headCells} rows={sampleData} />
-                <Stack direction={'column'} alignItems={'flex-start'} justifyContent={'center'} className="book-resource-box">
-                    <Typography variant="h3">Book Resources</Typography>
-                    <SelectOptions label="Select a book" options={[]} onChange={() => { }} />
-                </Stack>
-                <Stack direction={'row'} className="divider mt-2 mb-2"></Stack>
-                <Stack direction={'column'} className="bottom-action-btn-container full-width">
-                    <ActionButtons />
-                </Stack>
-                <Stack direction={'row'} className="pb-2">
-                    <BackButton className="show-xs" />
-                </Stack>
-            </Container>
-            <PageFooter />
-        </Box>
-    )
+  const [isSideNavOpen, setSideNavOpenStatus] = useState(false);
+  return (
+    <Box component={'div'}>
+      <PageHeader openSideNav={() => setSideNavOpenStatus(true)} />
+      <SideNav
+        open={isSideNavOpen}
+        close={() => {
+          setSideNavOpenStatus(false);
+        }}
+      />
+      <br />
+      <Container component={'div'}>
+        <TopControls />
+      </Container>
+      <StyledDetailSection>
+        <StyledDivider />
+        <DataTable
+          expandableRowOnMobile={false}
+          headCells={headCells}
+          rows={sampleData}
+        />
+        <StyledBookResourceBox
+          direction={'column'}
+          alignItems={'flex-start'}
+          justifyContent={'center'}
+        >
+          <Typography variant="h3">Book Resources</Typography>
+          <SelectOptions
+            label="Select a book"
+            options={[]}
+            onChange={() => {}}
+          />
+        </StyledBookResourceBox>
+        <StyledDivider marginTop={3} marginBottom={3} />
+        <Stack
+          direction={'column'}
+          width={'50%'}
+          alignItems={'flex-end'}
+          sx={(theme) => ({
+            marginTop: '50px',
+            marginBottom: '50px',
+            [theme.breakpoints.down('sm')]: {
+              width: '100%',
+              marginTop: '25px',
+              marginBottom: '25px',
+            },
+          })}
+        >
+          <ActionButtons />
+        </Stack>
+        <StyledBackButtonContainer flexDirection={'row'} className="pb-2">
+          <BackButton />
+        </StyledBackButtonContainer>
+      </StyledDetailSection>
+      <PageFooter />
+    </Box>
+  );
 }
-export default EntryDetailPage
+
+const StyledDetailSection = styled(Container)(({ theme }) => ({
+  marginTop: '3rem',
+  '.MuiTableBody-root .MuiTableRow-root:first-child > .MuiTableCell-root': {
+    padding: '16px',
+    paddingLeft: '0px',
+    color: theme.palette.text['darker-gray'],
+    fontWeight: 700,
+    fontFamily: 'Helvetica',
+    fontSize: '1.1rem',
+  },
+  '.MuiTableHead-root': {
+    display: 'none',
+  },
+  '.MuiTablePagination-root': {
+    display: 'none',
+  },
+  [theme.breakpoints.down('sm')]: {
+    '.MuiTableContainer-root .MuiTable-root': {
+      width: '100%',
+    },
+  },
+}));
+const StyledDivider = styled(Stack)(({ theme }) => ({
+  flexDirection: 'row',
+  height: 3,
+  backgroundColor: theme.palette.background['turquoise-light'],
+}));
+const StyledBookResourceBox = styled(Stack)(({ theme }) => ({
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+  backgroundColor: theme.palette.background['light-gray'],
+  padding: '20px',
+  paddingTop: '25px',
+  '.MuiTypography-h3': {
+    fontWeight: 700,
+    fontFamily: 'Helvetica',
+    marginBottom: '5px',
+  },
+  '.MuiOutlinedInput-root': {
+    width: '272px',
+    '.MuiSelect-outlined': {
+      paddingTop: '10px',
+      paddingBottom: '10px',
+    },
+  },
+  [theme.breakpoints.down('sm')]: {
+    '.MuiOutlinedInput-root': {
+      width: '100%',
+    },
+  },
+}));
+const StyledBackButtonContainer = styled(Stack)(({ theme }) => ({
+  flexDirection: 'row',
+  paddingBottom: '1.5rem',
+  display: 'none',
+  [theme.breakpoints.down('sm')]: {
+    display: 'flex',
+  },
+}));
+
+export default EntryDetailPage;
