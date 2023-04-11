e6c978159ea466a6bdaa9d130477c18e3bd669de
hiroshi <hiroshi.tan57@gmail.com>
2023-04-11 21:31:28 +0000

    Modify ButtonList

--- packages/ui-kit/src/crowd-bible/ButtonList/ButtonList.tsx
+++ packages/ui-kit/src/crowd-bible/ButtonList/ButtonList.tsx
@@ -1,48 +1,150 @@
-import React, { Fragment, ReactNode } from 'react';
+import React, { useState, ChangeEventHandler, ReactNode } from 'react';
+
+import { CiSearch } from '../../icons';
+import { SearchInput } from '../../input';
 
 import {
-  Box,
+  Stack,
+  IconButton,
   List,
-  Divider,
+  ListSubheader,
+  ListItemIcon,
   ListItemButton,
-  FormLabel,
   ListItemText,
+  Typography,
+  Divider,
 } from '@mui/material';
 
 type Item = {
-  label: string;
   value: unknown;
+  label: string;
 };
 
+type ListItemComProps = {
+  onClick(): void;
+  label: string;
+  itemIcon?: ReactNode;
+  withUnderline?: boolean;
+};
+
+function ListItemCom({
+  onClick,
+  label,
+  itemIcon,
+  withUnderline,
+}: ListItemComProps) {
+  const iconCom = itemIcon ? <ListItemIcon>{itemIcon}</ListItemIcon> : null;
+
+  const underlineCom = withUnderline ? (
+    <Divider sx={{ margin: '0 20px' }} />
+  ) : null;
+
+  return (
+    <>
+      <ListItemButton
+        // sx={{ paddingLeft: 0, paddingRight: 0 }}
+        onClick={onClick}
+      >
+        {iconCom}
+        <ListItemText primary={label} />
+      </ListItemButton>
+      {underlineCom}
+    </>
+  );
+}
+
 type ButtonListProps = {
+  label: string;
+  search?: {
+    value: string;
+    onChange(str: string): void;
+    placeHolder: string;
+  };
+  toolBtnGroup?: ReactNode;
   withUnderline?: boolean;
   items: Item[];
-  label: string;
-  onClick(selected: unknown): void;
-  Icon?: ReactNode | null;
+  onClick(value: unknown): void;
+  itemIcon?: ReactNode;
 };
 
 export function ButtonList({
+  label,
+  search,
+  toolBtnGroup,
   withUnderline = false,
   items,
   onClick,
-  label,
-  Icon,
+  itemIcon,
 }: ButtonListProps) {
+  const [isShownSearchInput, setIsShownSearchInput] = useState<boolean>(false);
+
+  const handleToggleSearchInput = () => {
+    setIsShownSearchInput((shown) => !shown);
+  };
+
+  const handleChangeSearchInput: ChangeEventHandler<HTMLInputElement> = (
+    event,
+  ) => {
+    search?.onChange(event.target.value);
+  };
+
+  const searchBtnCom = search ? (
+    <IconButton onClick={handleToggleSearchInput}>
+      <CiSearch />
+    </IconButton>
+  ) : null;
+
+  const searchInputCom =
+    search && isShownSearchInput ? (
+      <SearchInput
+        label={search.placeHolder}
+        value={search.value}
+        onChange={handleChangeSearchInput}
+        fullWidth
+        sx={{ padding: '20px 0' }}
+      />
+    ) : null;
+
+  const toolBtnComs =
+    search || toolBtnGroup ? (
+      <>
+        {searchBtnCom}
+        {toolBtnGroup ? toolBtnGroup : null}
+      </>
+    ) : null;
+
   return (
-    <Box sx={{ width: '100%' }}>
-      <List>
-        <FormLabel color="gray">{label}</FormLabel>
-        {items.map(({ label, value }) => (
-          <Fragment key={label}>
-            <ListItemButton onClick={() => onClick(value)}>
-              <ListItemText primary={label} color="dark" />
-              {Icon ? Icon : null}
-            </ListItemButton>
-            {withUnderline ? <Divider /> : null}
-          </Fragment>
-        ))}
-      </List>
-    </Box>
+    <List
+      component="nav"
+      sx={{ padding: '20px' }}
+      subheader={
+        <ListSubheader
+          component="div"
+          sx={{ padding: '6px 20px', backgroundColor: 'transparent' }}
+        >
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Typography variant="overline" sx={{ opacity: 0.5 }}>
+              {label}
+            </Typography>
+            {toolBtnComs}
+          </Stack>
+          {searchInputCom}
+        </ListSubheader>
+      }
+    >
+      {items.map(({ value, label }) => (
+        <ListItemCom
+          key={label}
+          label={label}
+          onClick={() => onClick(value)}
+          itemIcon={itemIcon}
+          withUnderline={withUnderline}
+        />
+      ))}
+    </List>
   );
 }
