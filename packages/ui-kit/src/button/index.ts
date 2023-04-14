bab32cc213dccdb01ef74b42abd80991be1178bb
hiroshi <hiroshi.tan57@gmail.com>
2023-04-14 12:13:42 +0000

    Add FilterButton

--- packages/ui-kit/src/button/index.ts
+++ packages/ui-kit/src/button/index.ts
@@ -1,6 +1,7 @@
 export { Button } from './Button';
 export { AgreeConfirmButton } from './AgreeConfirmButton';
 export { PlusButton } from './PlusButton';
+export { FilterButton } from './FilterButton';
 export { CircleButton } from './CircleButton';
 export { ScrollDownButton } from './ScrollDownButton';
 export { AddAttachmentButton } from './AddAttachmentButton';
