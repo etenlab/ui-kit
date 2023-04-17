0942432cad4d7370aa05b42f229aa3875ec1a9a2
hiroshi <hiroshi.tan57@gmail.com>
2023-04-17 23:33:14 +0000

    Add Typography.stories

--- stories/primary/typography/typography.stories.tsx
+++ stories/primary/typography/typography.stories.tsx
@@ -28,59 +28,219 @@ const Template: ComponentStory<typeof Typography> = (args) => (
 export const H1 = Template.bind({});
 H1.args = {
   variant: 'h1',
-  children: text,
+  children: (
+    <>
+      font-weight: 600
+      <br />
+      font-size: 28
+      <br />
+      line-height: 36px
+      <br />
+      letter-spacing: -0.02em
+      <br />
+      text-transform: capitalize
+      <br />
+      <br />
+      <br />
+      {text}
+    </>
+  ),
 };
 
 export const H2 = Template.bind({});
 H2.args = {
   variant: 'h2',
-  children: text,
+  children: (
+    <>
+      font-weight: 600
+      <br />
+      font-size: 20
+      <br />
+      line-height: 28px
+      <br />
+      letter-spacing: -0.02em
+      <br />
+      text-transform: capitalize
+      <br />
+      <br />
+      <br />
+      {text}
+    </>
+  ),
 };
 
 export const H3 = Template.bind({});
 H3.args = {
   variant: 'h3',
-  children: text,
+  children: (
+    <>
+      font-weight: 600
+      <br />
+      font-size: 18
+      <br />
+      line-height: 28px
+      <br />
+      letter-spacing: -0.02em
+      <br />
+      text-transform: capitalize
+      <br />
+      <br />
+      <br />
+      {text}
+    </>
+  ),
 };
 
 export const Subtitle1 = Template.bind({});
 Subtitle1.args = {
   variant: 'subtitle1',
-  children: text,
+  children: (
+    <>
+      font-weight: 600
+      <br />
+      font-size: 14
+      <br />
+      line-height: 20px display: flex
+      <br />
+      align-Ttems: center
+      <br />
+      <br />
+      <br />
+      {text}
+    </>
+  ),
 };
 
 export const Subtitle2 = Template.bind({});
 Subtitle2.args = {
   variant: 'subtitle2',
-  children: text,
+  children: (
+    <>
+      font-weight: 700
+      <br />
+      font-size: 14
+      <br />
+      line-height: 18px display: flex
+      <br />
+      align-Ttems: center
+      <br />
+      <br />
+      <br />
+      {text}
+    </>
+  ),
 };
 
 export const Body1 = Template.bind({});
 Body1.args = {
   variant: 'body1',
-  children: text,
+  children: (
+    <>
+      font-weight: 400
+      <br />
+      font-size: 16
+      <br />
+      line-height: 26px
+      <br />
+      display: flex
+      <br />
+      align-items: center
+      <br />
+      <br />
+      <br />
+      {text}
+    </>
+  ),
 };
 
 export const Body2 = Template.bind({});
 Body2.args = {
   variant: 'body2',
-  children: text,
+  children: (
+    <>
+      font-weight: 400
+      <br />
+      font-size: 14
+      <br />
+      line-height: 20px
+      <br />
+      display: flex
+      <br />
+      align-items: center
+      <br />
+      <br />
+      <br />
+      {text}
+    </>
+  ),
 };
 
 export const Body3 = Template.bind({});
 Body3.args = {
   variant: 'body3',
-  children: text,
+  children: (
+    <>
+      font-weight: 400
+      <br />
+      font-size: 12
+      <br />
+      line-height: 20px
+      <br />
+      display: flex
+      <br />
+      align-items: center
+      <br />
+      <br />
+      <br />
+      {text}
+    </>
+  ),
 };
 
 export const Caption = Template.bind({});
 Caption.args = {
   variant: 'caption',
-  children: text,
+  children: (
+    <>
+      font-weight: 400
+      <br />
+      font-size: 14
+      <br />
+      line-height: 30px
+      <br />
+      display: flex
+      <br />
+      align-items: center
+      <br />
+      <br />
+      <br />
+      {text}
+    </>
+  ),
 };
 
 export const Overline = Template.bind({});
 Overline.args = {
   variant: 'overline',
-  children: text,
+  children: (
+    <>
+      font-weight: 600
+      <br />
+      font-size: 14
+      <br />
+      line-height: 20px
+      <br />
+      display: flex
+      <br />
+      align-items: center
+      <br />
+      letter-spacing: 0.05em
+      <br />
+      text-transform: uppercase
+      <br />
+      <br />
+      <br />
+      {text}
+    </>
+  ),
 };
