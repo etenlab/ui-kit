42f57233cf9b3fc5343242365cef2c280f98888c
LeO <devgru0114@gmail.com>
2023-05-22 16:30:16 -0400

    add react-spinner

---
+++ packages/ui-kit/src/FadeSpinner/FadeSpinner.tsx
@@ -0,0 +1,23 @@
+import { Stack, Typography } from '@mui/material';
+import React from 'react';
+
+import { FadeLoader } from 'react-spinners';
+
+export const FadeSpinner = ({
+  color,
+  progress,
+}: {
+  color: string;
+  progress: string;
+}) => {
+  return (
+    <Stack direction="column" alignItems="center" sx={{ gap: '20px' }}>
+      <FadeLoader
+        color={color}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      ></FadeLoader>
+      <Typography variant="body1">{progress}% Loading...</Typography>
+    </Stack>
+  );
+};
