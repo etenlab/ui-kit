6b7aef715b5dd9520899486ac11898bde0f348a0
hiroshi <hiroshi.tan57@gmail.com>
2023-06-02 06:49:40 +0000

    refactor(CardMenu): alignment with figma design

--- packages/ui-kit/src/crowd-bible/CardsMenu/CardsMenu.tsx
+++ packages/ui-kit/src/crowd-bible/CardsMenu/CardsMenu.tsx
@@ -1,146 +1,91 @@
-
-import { CiSearch } from '../../icons';
-import { List, ListItemButton, Paper, Grid, Typography, ListSubheader, Stack, IconButton, Divider } from '@mui/material';
-import React, { useState, ChangeEventHandler, ReactNode } from 'react';
+import {
+  List,
+  ListItem,
+  Button,
+  Typography,
+  ListSubheader,
+  Stack,
+} from '@mui/material';
+import React, { ReactNode } from 'react';
 import { useColorModeContext } from '../../ThemeProvider';
-import { SearchInput } from '../../input';
 
 export type CardsMenuItemType = {
   value: string;
-  label: ReactNode;
-  startIcon?: ReactNode;
-  description?: string,
-  endIcon?: ReactNode;
-  color?: string;
+  title: ReactNode;
+  description?: string;
+  startIcon: ReactNode;
   disabled?: boolean;
-};
-
-type CardsMenuProps = {
-  label: string;
-  toolBtnGroup?: ReactNode;
-  withUnderline?: boolean;
-  onClick(value: string): void;
-  search?: {
-    value: string;
-    onChange(str: string): void;
-    placeHolder: string;
-  };
-  items: CardsMenuItemType[];
 };
 
 type CardsMenutemProps = {
   item: CardsMenuItemType;
-  withUnderline?: boolean;
-  onClick(): void;
+  onClick(value: string): void;
 };
 
-function CardsMenutem({ item, withUnderline, onClick }: CardsMenutemProps) {
-  const { label, description, startIcon, disabled } = item;
+function CardsMenutem({ item, onClick }: CardsMenutemProps) {
+  const { getColor } = useColorModeContext();
 
-  const underlineCom = withUnderline ? (
-    <Divider sx={{ margin: '0 20px' }} />
-  ) : null;
+  const { title, description, startIcon, disabled, value } = item;
 
   return (
-    <>
-      <ListItemButton
-          onClick={onClick}
-          disabled={disabled}
+    <ListItem sx={{ padding: '6px 20px' }}>
+      <Button
+        variant="outlined"
+        onClick={() => {
+          onClick(value);
+        }}
+        disabled={disabled}
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'flex-start',
+          padding: '14px 12px',
+          paddingLeft: '24px',
+          gap: '10px',
+          width: '100%',
+          borderColor: getColor('middle-gray'),
+          borderRadius: '10px',
+          backgroundColor: getColor('disable'),
+          textTransform: 'none',
+          textAlign: 'start',
+        }}
+      >
+        {startIcon}
+        <Stack
+          gap="5px"
+          direction="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
         >
-          <Paper
-            sx={{
-              p: 4,
-              margin: 'auto',
-              border: '1px solid #C2CBD7',
-              flexGrow: 1,
-              backgroundColor: '#F3F6F9',
-            }}
+          <Typography
+            variant="subtitle1"
+            color="text.dark"
+            sx={{ display: 'inline-block' }}
           >
-            <Grid container spacing={2}>
-              <Grid item>
-                {startIcon}
-              </Grid>
-              <Grid item xs={12} sm container>
-                <Grid item xs container direction="column" spacing={2}>
-                  <Grid item xs>
-                    <Typography
-                      fontSize="18px"
-                      gutterBottom
-                      variant="subtitle1"
-                      component="div"
-                    >
-                      {label}
-                    </Typography>
-                    <Typography variant="body2" gutterBottom>
-                      {description}
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Paper>
-        </ListItemButton>
-      {underlineCom}
-    </>
+            {title}
+          </Typography>
+          <Typography variant="body2" sx={{ display: 'inline-block' }}>
+            {description}
+          </Typography>
+        </Stack>
+      </Button>
+    </ListItem>
   );
 }
 
+type CardsMenuProps = {
+  label: string;
+  onClick(value: string): void;
+  items: CardsMenuItemType[];
+};
 
-export function CardsMenu({
-  label,
-  search,
-  toolBtnGroup,
-  withUnderline = false,
-  items,
-  onClick,
-}: CardsMenuProps) {
+export function CardsMenu({ label, items, onClick }: CardsMenuProps) {
   const { getColor } = useColorModeContext();
-
-  const [isShownSearchInput, setIsShownSearchInput] = useState<boolean>(false);
-
-  const handleToggleSearchInput = () => {
-    setIsShownSearchInput((shown) => !shown);
-  };
-
-  const handleChangeSearchInput: ChangeEventHandler<HTMLInputElement> = (
-    event,
-  ) => {
-    search?.onChange(event.target.value);
-  };
-
-  const searchBtnCom = search ? (
-    <IconButton onClick={handleToggleSearchInput}>
-      <CiSearch />
-    </IconButton>
-  ) : null;
-
-  const searchInputCom =
-    search && isShownSearchInput ? (
-      <SearchInput
-        label={search.placeHolder}
-        value={search.value}
-        onChange={handleChangeSearchInput}
-        fullWidth
-        sx={{ padding: '20px 0' }}
-      />
-    ) : null;
-
-  const toolBtnComs =
-    search || toolBtnGroup ? (
-      <Stack
-        direction="row"
-        gap="16px"
-        justifyContent="flex-start"
-        alignItems="center"
-      >
-        {searchBtnCom}
-        {toolBtnGroup ? toolBtnGroup : null}
-      </Stack>
-    ) : null;
 
   return (
     <List
-    component="nav"
+      component="nav"
       sx={{ padding: '0' }}
       subheader={
         <ListSubheader
@@ -155,23 +100,13 @@ export function CardsMenu({
             <Typography variant="overline" sx={{ opacity: 0.5 }}>
               {label}
             </Typography>
-            {toolBtnComs}
           </Stack>
-          {searchInputCom}
         </ListSubheader>
       }
-      >
-        {items.map((item) => {
-        return (
-          <CardsMenutem
-            item={item}
-            key={item.value}
-            onClick={() => onClick(item.value)}
-            withUnderline={withUnderline}
-          />
-        );
-      })}
-      </List>
+    >
+      {items.map((item) => (
+        <CardsMenutem item={item} key={item.value} onClick={onClick} />
+      ))}
+    </List>
   );
-  
 }
--- stories/partial/crowd-bible/CardsMenu/CardsMenu.stories.tsx
+++ stories/partial/crowd-bible/CardsMenu/CardsMenu.stories.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { logger, ThemeProvider } from '../../../../packages/ui-kit/src';
+import { ThemeProvider } from '../../../../packages/ui-kit/src';
 import { buildDocs } from '../../../common';
 import { StoryPaper } from '../../../StoryPaper';
 
 import { CardsMenu } from '../../../../packages/ui-kit/src/crowd-bible';
 import {
-  PlusButton,
-  BiFile,
-  FilterButton,
+  DiFileText,
+  DiAskQuestion,
+  DiTranslate,
+  DiCross,
 } from '../../../../packages/ui-kit/src';
-
-import { Chip } from '@mui/material';
 
 export default {
   title: 'Partial/Crowd Bible/CardsMenu',
@@ -34,148 +33,45 @@ const Template: ComponentStory<typeof CardsMenu> = (args) => (
 
 const items = [
   {
-    value: '1',
-    label: 'Document #1',
-    description: 'Document #1 Description',
+    value: '/documents',
+    title: 'Documents',
+    description: 'Upload a document to use in other crowd sourcing tools',
+    startIcon: <DiFileText color="blue" />,
   },
   {
-    value: '2',
-    label: 'Document #2',
-    description: 'Document #2 Description',
+    value: '/qa-route',
+    title: 'Question & Answer',
+    description:
+      'Annotate a text with questions so other users can provide answers',
+    startIcon: <DiAskQuestion color="blue" />,
   },
   {
-    value: '3',
-    label: 'Document #3',
-    description: 'Document #3 Description',
+    value: '/translation',
+    title: 'Translation',
+    description:
+      'Translate a document by submitting and voting on translations',
+    startIcon: <DiTranslate color="blue" />,
+    disabled: true,
+  },
+  {
+    value: '/commentary',
+    title: 'Commentary',
+    description: 'Annotate a document and vote on what you support',
+    startIcon: <DiCross color="red" />,
   },
 ];
-
-const docItems = items.map((item) => ({
-  ...item,
-  startIcon: (
-    <BiFile
-      style={{
-        borderRadius: '7px',
-        padding: '7px',
-        fontSize: '32px',
-        background: '#E3EAF3',
-      }}
-    />
-  ),
-}));
 
 export const CardsMenuList1 = Template.bind({});
 CardsMenuList1.storyName = 'Document List';
 CardsMenuList1.args = {
-  label: 'List of Docs',
-  search: {
-    value: '',
-    onChange: (str: string) => {
-      logger.info(str);
-    },
-    placeHolder: 'Search documents by name',
-  },
-  withUnderline: true,
-  items: docItems,
-  toolBtnGroup: (
-    <>
-      <FilterButton variant="secondary" onClick={() => {}} />
-      <PlusButton variant="primary" onClick={() => {}} />
-    </>
-  ),
-  onClick: (selected: unknown) => alert(`Clicked ${selected} Button`),
+  label: 'Document Tools',
+  items: items,
+  onClick: (selected: string) => alert(`Clicked ${selected} Button`),
 };
 CardsMenuList1.parameters = buildDocs(
   <CardsMenu
-    label="List of Docs"
-    search={{
-      value: '',
-      onChange: (str: string) => {
-        logger.info(str);
-      },
-      placeHolder: 'Search documents by name',
-    }}
-    withUnderline={true}
+    label="Document Tools"
     items={items}
-    // toolBtnGroup={
-    //   <Fragment>
-    //     <FilterButton variant="secondary" onClick={() => {}} />
-    //     <PlusButton variant="primary" onClick={() => {}} />
-    //   </Fragment>
-    // }
     onClick={(selected: unknown) => alert(`Clicked ${selected} Button`)}
-  />,
-);
-
-export const CardsMenuList2 = Template.bind({});
-CardsMenuList2.storyName = 'Feedback Chapter List';
-CardsMenuList2.args = {
-  label: 'Select a chapter',
-  withUnderline: true,
-  items,
-  onClick: (selected: unknown) => alert(`Clicked ${selected} Button`),
-};
-CardsMenuList2.parameters = buildDocs(
-  <CardsMenu
-    label="Select a chapter"
-    withUnderline={true}
-    items={items}
-    onClick={() => {}}
-  />,
-);
-
-export const CardsMenuList3 = Template.bind({});
-CardsMenuList3.storyName = 'Site Text Application List';
-CardsMenuList3.args = {
-  label: 'List of Applications',
-  withUnderline: true,
-  items,
-  toolBtnGroup: <PlusButton variant="primary" onClick={() => {}} />,
-  onClick: (selected: unknown) => alert(`Clicked ${selected} Button`),
-};
-CardsMenuList3.parameters = buildDocs(
-  <CardsMenu
-    label="Select a chapter"
-    withUnderline={true}
-    items={items}
-    toolBtnGroup={<PlusButton variant="primary" onClick={() => {}} />}
-    onClick={() => {}}
-  />,
-);
-
-const HomeItems = [
-  { value: '1', label: 'Documents viewer', color: 'green', endIcon: '1' },
-  { value: '2', label: 'Feedback', startIcon: 'selected' },
-  {
-    value: '3',
-    label: 'Versification editor',
-    endIcon: (
-      <Chip
-        label="Online only"
-        variant="outlined"
-        color="error"
-        size="small"
-        sx={{ marginLeft: 2 }}
-      />
-    ),
-    disabled: true,
-    color: 'red',
-  },
-];
-
-export const CardsMenuList4 = Template.bind({});
-CardsMenuList4.storyName = 'Home Route List';
-CardsMenuList4.args = {
-  label: 'Document tools',
-  withUnderline: true,
-  items: HomeItems,
-  onClick: (selected: unknown) => alert(`Clicked ${selected} Button`),
-};
-CardsMenuList4.parameters = buildDocs(
-  <CardsMenu
-    label="Select a chapter"
-    withUnderline={true}
-    items={HomeItems}
-    onClick={() => {}}
   />,
 );
