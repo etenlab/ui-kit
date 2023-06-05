69b67eead36a511160fc9969b887fb1f1f19cc1c
dia <takeshisuz57@gmail.com>
2023-06-05 12:38:33 +0000

    refactor(icons): refactor icons with new design icons

--- packages/ui-kit/src/icons/index.ts
+++ packages/ui-kit/src/icons/index.ts
@@ -1,58 +1,15 @@
-export {
-  CiRead,
-  CiDark,
-  CiLight,
-  CiSearch,
-  CiPause1,
-  CiUnread,
-  CiFaceSmile,
-  CiCircleRemove,
-} from 'react-icons/ci';
+export { CiDark, CiLight, CiCircleRemove } from 'react-icons/ci';
 export {
   BiLike,
-  BiFile,
-  BiUser,
   BiShare,
   BiImages,
   BiPencil,
   BiDislike,
-  BiTrashAlt,
-  BiDownload,
   BiChevronUp,
-  BiCommentAdd,
-  BiChevronDown,
   BiCheckCircle,
-  BiLeftArrowAlt,
   BiChevronsDown,
-  BiRightArrowAlt,
-  BiMessageRounded,
-  BiDotsHorizontalRounded,
-  BiVolumeFull,
 } from 'react-icons/bi';
-export { BsFillPlayFill, BsClock, BsFillRecordFill } from 'react-icons/bs';
-export {
-  FiX,
-  FiMic,
-  FiBell,
-  FiPlus,
-  FiMenu,
-  FiEdit,
-  FiCheck,
-  FiPause,
-  FiFilter,
-  FiColumns,
-  FiPaperclip,
-  FiCreditCard,
-  FiCodesandbox,
-  FiCornerDownLeft,
-} from 'react-icons/fi';
-export {
-  TbLetterT,
-  TbCameraRotate,
-  TbVideoPlus,
-  TbLayoutColumns,
-  TbLayoutRows,
-  TbArrowBack,
-} from 'react-icons/tb';
+export { BsFillRecordFill } from 'react-icons/bs';
+export { FiCodesandbox } from 'react-icons/fi';
 export { IoChatboxOutline, IoCloseCircle } from 'react-icons/io5';
 export * from './DesignedIcons';
--- stories/primary/icons/icons.stories.tsx
+++ stories/primary/icons/icons.stories.tsx
@@ -2,56 +2,16 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import {
-  CiRead,
   CiDark,
   CiLight,
-  CiPause1,
-  CiUnread,
-  CiSearch,
-  CiFaceSmile,
-  CiCircleRemove,
-  BiLeftArrowAlt,
-  BiRightArrowAlt,
-  BiMessageRounded,
-  BiChevronDown,
-  BiChevronUp,
-  BiDownload,
-  BiTrashAlt,
-  BiFile,
   BiDislike,
   BiLike,
-  BiCommentAdd,
-  BiUser,
-  BiCheckCircle,
-  BiDotsHorizontalRounded,
   BiShare,
   BiPencil,
   BiImages,
   BiChevronsDown,
-  BiVolumeFull,
-  BsFillPlayFill,
   BsFillRecordFill,
-  BsClock,
-  FiBell,
   FiCodesandbox,
-  FiCheck,
-  FiFilter,
-  FiMic,
-  FiPlus,
-  FiX,
-  FiPause,
-  FiMenu,
-  FiColumns,
-  FiCreditCard,
-  FiEdit,
-  FiPaperclip,
-  FiCornerDownLeft,
-  TbLetterT,
-  TbCameraRotate,
-  TbVideoPlus,
-  TbLayoutColumns,
-  TbLayoutRows,
-  TbArrowBack,
   DiViewer,
   DiAdd,
   DiAdmin,
@@ -287,59 +247,16 @@ const Template: ComponentStory<typeof Div> = () => {
       </Div>
       <hr />
       <Div>
-        <TitleWithIcon title="CiUnread" icon={<CiUnread />} />
-        <TitleWithIcon title="CiRead" icon={<CiRead />} />
-        <TitleWithIcon title="CiPause1" icon={<CiPause1 />} />
         <TitleWithIcon title="CiDark" icon={<CiDark />} />
         <TitleWithIcon title="CiLight" icon={<CiLight />} />
-        <TitleWithIcon title="CiSearch" icon={<CiSearch />} />
-        <TitleWithIcon title="CiFaceSmile" icon={<CiFaceSmile />} />
-        <TitleWithIcon title="CiCircleRemove" icon={<CiCircleRemove />} />
-        <TitleWithIcon title="BiLeftArrowAlt" icon={<BiLeftArrowAlt />} />
-        <TitleWithIcon title="BiRightArrowAlt" icon={<BiRightArrowAlt />} />
-        <TitleWithIcon title="BiMessageRounded" icon={<BiMessageRounded />} />
-        <TitleWithIcon title="BiChevronDown" icon={<BiChevronDown />} />
-        <TitleWithIcon title="BiChevronUp" icon={<BiChevronUp />} />
-        <TitleWithIcon title="BiTrashAlt" icon={<BiTrashAlt />} />
-        <TitleWithIcon title="BiFile" icon={<BiFile />} />
         <TitleWithIcon title="BiDislike" icon={<BiDislike />} />
         <TitleWithIcon title="BiLike" icon={<BiLike />} />
-        <TitleWithIcon title="BiCommentAdd" icon={<BiCommentAdd />} />
-        <TitleWithIcon title="BiUser" icon={<BiUser />} />
-        <TitleWithIcon title="BiCheckCircle" icon={<BiCheckCircle />} />
-        <TitleWithIcon title="BiDownload" icon={<BiDownload />} />
         <TitleWithIcon title="BiChevronsDown" icon={<BiChevronsDown />} />
-        <TitleWithIcon
-          title="BiDotsHorizontalRounded"
-          icon={<BiDotsHorizontalRounded />}
-        />
         <TitleWithIcon title="BiShare" icon={<BiShare />} />
         <TitleWithIcon title="BiPencil" icon={<BiPencil />} />
         <TitleWithIcon title="BiImages" icon={<BiImages />} />
-        <TitleWithIcon title="BiVolumeFull" icon={<BiVolumeFull />} />
-        <TitleWithIcon title="BsFillPlayFill" icon={<BsFillPlayFill />} />
         <TitleWithIcon title="BsFillRecordFill" icon={<BsFillRecordFill />} />
-        <TitleWithIcon title="BsClock" icon={<BsClock />} />
-        <TitleWithIcon title="FiBell" icon={<FiBell />} />
         <TitleWithIcon title="FiCodesandbox" icon={<FiCodesandbox />} />
-        <TitleWithIcon title="FiCheck" icon={<FiCheck />} />
-        <TitleWithIcon title="FiFilter" icon={<FiFilter />} />
-        <TitleWithIcon title="FiMic" icon={<FiMic />} />
-        <TitleWithIcon title="FiPlus" icon={<FiPlus />} />
-        <TitleWithIcon title="FiX" icon={<FiX />} />
-        <TitleWithIcon title="FiPause" icon={<FiPause />} />
-        <TitleWithIcon title="FiMenu" icon={<FiMenu />} />
-        <TitleWithIcon title="FiColumns" icon={<FiColumns />} />
-        <TitleWithIcon title="FiCreditCard" icon={<FiCreditCard />} />
-        <TitleWithIcon title="FiEdit" icon={<FiEdit />} />
-        <TitleWithIcon title="FiPaperclip" icon={<FiPaperclip />} />
-        <TitleWithIcon title="FiCornerDownLeft" icon={<FiCornerDownLeft />} />
-        <TitleWithIcon title="TbLetterT" icon={<TbLetterT />} />
-        <TitleWithIcon title="TbCameraRotate" icon={<TbCameraRotate />} />
-        <TitleWithIcon title="TbVideoPlus" icon={<TbVideoPlus />} />
-        <TitleWithIcon title="TbLayoutColumns" icon={<TbLayoutColumns />} />
-        <TitleWithIcon title="TbLayoutRows" icon={<TbLayoutRows />} />
-        <TitleWithIcon title="TbArrowBack" icon={<TbArrowBack />} />
       </Div>
     </>
   );
