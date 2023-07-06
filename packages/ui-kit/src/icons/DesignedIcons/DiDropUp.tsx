198c4005804fba39a680cced9e7c6a0ec417ed95
hiroshi <hiroshi.tan57@gmail.com>
2023-07-06 12:22:44 +0000

    feat: add more design icons

---
+++ packages/ui-kit/src/icons/DesignedIcons/DiDropUp.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { SvgIcon, type SvgIconProps } from '@mui/material';
+
+export function DiDropUp(props: SvgIconProps) {
+  return (
+    <SvgIcon {...props}>
+      <g clip-path="url(#clip0_4047_24194)">
+        <path
+          d="M6 15L12 9L18 15"
+          stroke="currentColor"
+          fill="none"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4047_24194">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </SvgIcon>
+  );
+}
---
+++ packages/ui-kit/src/icons/DesignedIcons/DiReply.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { SvgIcon, type SvgIconProps } from '@mui/material';
+
+export function DiReply(props: SvgIconProps) {
+  return (
+    <SvgIcon {...props}>
+      <g clip-path="url(#clip0_4047_24173)">
+        <path
+          d="M13.0002 4V8C6.42519 9.028 3.98019 14.788 3.00019 20C2.96319 20.206 8.38419 14.038 13.0002 14V18L21.0002 11L13.0002 4Z"
+          stroke="currentColor"
+          fill="none"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4047_24173">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </SvgIcon>
+  );
+}
